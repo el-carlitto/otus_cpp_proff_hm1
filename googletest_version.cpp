3f71e9b4d09728ba5256a4d4e06b1c58ada65eb5
el-carlitto <nikita_2506@mail.ru>
2022-11-08 14:26:31 +0300

    fix test assertions

--- googletest_version.cpp
+++ googletest_version.cpp
@@ -1,9 +1,7 @@
 #include <gtest/gtest.h>
-
+#include "lib.h"
 // Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
+TEST(VersionTest, BasicAssertions) {
   // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+ASSERT_GT(version(), 0) << "wrong programm version";
 }
